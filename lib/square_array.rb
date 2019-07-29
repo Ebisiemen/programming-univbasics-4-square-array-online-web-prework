100c5a998ff9c754f6f9704fb29171e3c53dfcf1
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-07-29 14:37:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = []
   counter = 0 
   while counter < array.length do 
-    new_array << (array)
+    new_array << (array[])
     puts array[counter]
     counter +=1 
   end
